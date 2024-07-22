7c020105ecd9e7009e47badc5975583ec5fceb85
shixinwu <shixinwu16@gmail.com>
2024-07-22 17:29:32 -0400

    removed commented out code from xdmod entrypoint; added a new bash script that runs xdmod and ondemand from start to finish; changed ondemand docker-compose file to take in a different xdmod image for mariadb so that it stays consistent with the others

--- xdmod/entrypoint.sh
+++ xdmod/entrypoint.sh
@@ -101,85 +101,44 @@ then
 
   # ensure php command-line errors are logged to a file
   sed -i 's/^;error_log = php_errors.log/error_log = \/var\/log\/php_errors.log/' /etc/php.ini
+  xdmod-import-csv -t hierarchy -i $REF_DIR/hierarchy.csv
+  xdmod-import-csv -t group-to-hierarchy -i $REF_DIR/group-to-hierarchy.csv
 
-#  user=root
-#  pass=''
-#  tableexist=$(mysql -u${user} -p${pass} --host mariadb -e "SHOW DATABASES LIKE 'modw'")
-   if [[ -z "$tableexist" ]]; then
-##    dnf install -y ~/rpmbuild/RPMS/*/*.rpm
-#    copy_template_httpd_conf
-#    mysql -h mariadb -e "CREATE USER 'root'@'xdmod' IDENTIFIED BY '';
-#    GRANT ALL PRIVILEGES ON *.* TO 'root'@'xdmod' WITH GRANT OPTION;
-#    FLUSH PRIVILEGES;"
-#
-#    # TODO: Replace diff files with hard fixes
-#    # Modify integration sso tests to work with cloud realm
-#    echo "building databases"
-#    if [ "$XDMOD_REALMS" = "cloud" ]; then
-#      if ! patch --dry-run -Rfsup1 --directory=$REPODIR < $BASEDIR/diff/SSOLoginTest.php.diff >/dev/null; then
-#        # -- Fix users searched in SSO test
-#        patch -up1 --directory=$REPODIR < $BASEDIR/diff/SSOLoginTest.php.diff
-#      fi
-#    else
-#      if patch --dry-run -Rfsup1 --directory=$REPODIR < $BASEDIR/diff/SSOLoginTest.php.diff >/dev/null; then
-#        # -- Reverse previous patch
-#        patch -R -up1 --directory=$REPODIR < $BASEDIR/diff/SSOLoginTest.php.diff
-#      fi
-#    fi
-#
-#    expect $BASEDIR/scripts/xdmod-setup-start.tcl | col -b
-#
-#    if [[ "$XDMOD_REALMS" == *"jobs"* ]]; then
-#      expect $BASEDIR/scripts/xdmod-setup-jobs.tcl | col -b
-#    fi
-#    if [[ "$XDMOD_REALMS" == *"storage"* ]]; then
-#      expect $BASEDIR/scripts/xdmod-setup-storage.tcl | col -b
-#    fi
-#    if [[  "$XDMOD_REALMS" == *"cloud"* ]]; then
-#      expect $BASEDIR/scripts/xdmod-setup-cloud.tcl | col -b
-#    fi
-#
-#    expect $BASEDIR/scripts/xdmod-setup-finish.tcl | col -b
-
-    xdmod-import-csv -t hierarchy -i $REF_DIR/hierarchy.csv
-    xdmod-import-csv -t group-to-hierarchy -i $REF_DIR/group-to-hierarchy.csv
-
-    if [[ "$XDMOD_REALMS" == *"jobs"* ]];
-    then
-      for resource in $REF_DIR/*.log; do
-        sudo -u xdmod xdmod-shredder -r `basename $resource .log` -f slurm -i $resource;
-      done
-    fi
-
-    set_resource_spec_end_times
-    sudo -u xdmod xdmod-ingestor
-
-    if [[ "$XDMOD_REALMS" == *"cloud"* ]];
-    then
-      last_modified_start_date=$(date +'%F %T')
-      sudo -u xdmod xdmod-shredder -r openstack -d $REF_DIR/openstack -f openstack
-      sudo -u xdmod xdmod-shredder -r nutsetters -d $REF_DIR/nutsetters -f openstack
-
-      sudo -u xdmod xdmod-import-csv -t cloud-project-to-pi -i $REF_DIR/cloud-pi-test.csv
-      sudo -u xdmod xdmod-shredder -r openstack -d $REF_DIR/openstack_error_sessions -f openstack
-      sudo -u xdmod xdmod-ingestor --datatype openstack
-      sudo -u xdmod xdmod-ingestor --aggregate=cloud --last-modified-start-date "$last_modified_start_date"
-    fi
-
-    if [[ "$XDMOD_REALMS" == *"storage"* ]];
-    then
-      for storage_dir in $REF_DIR/storage/*; do
-          sudo -u xdmod xdmod-shredder -f storage -r $(basename $storage_dir) -d $storage_dir
-      done
-      last_modified_start_date=$(date +'%F %T')
-      sudo -u xdmod xdmod-ingestor --datatype storage
-      sudo -u xdmod xdmod-ingestor --aggregate=storage --last-modified-start-date "$last_modified_start_date"
-    fi
-
-    sudo -u xdmod xdmod-import-csv -t names -i $REF_DIR/names.csv
-    sudo -u xdmod xdmod-ingestor
-    php $BASEDIR/scripts/create_xdmod_users.php
+  if [[ "$XDMOD_REALMS" == *"jobs"* ]];
+  then
+    for resource in $REF_DIR/*.log; do
+      sudo -u xdmod xdmod-shredder -r `basename $resource .log` -f slurm -i $resource;
+    done
   fi
+
+  set_resource_spec_end_times
+  sudo -u xdmod xdmod-ingestor
+
+  if [[ "$XDMOD_REALMS" == *"cloud"* ]];
+  then
+    last_modified_start_date=$(date +'%F %T')
+    sudo -u xdmod xdmod-shredder -r openstack -d $REF_DIR/openstack -f openstack
+    sudo -u xdmod xdmod-shredder -r nutsetters -d $REF_DIR/nutsetters -f openstack
+
+    sudo -u xdmod xdmod-import-csv -t cloud-project-to-pi -i $REF_DIR/cloud-pi-test.csv
+    sudo -u xdmod xdmod-shredder -r openstack -d $REF_DIR/openstack_error_sessions -f openstack
+    sudo -u xdmod xdmod-ingestor --datatype openstack
+    sudo -u xdmod xdmod-ingestor --aggregate=cloud --last-modified-start-date "$last_modified_start_date"
+  fi
+
+  if [[ "$XDMOD_REALMS" == *"storage"* ]];
+  then
+    for storage_dir in $REF_DIR/storage/*; do
+        sudo -u xdmod xdmod-shredder -f storage -r $(basename $storage_dir) -d $storage_dir
+    done
+    last_modified_start_date=$(date +'%F %T')
+    sudo -u xdmod xdmod-ingestor --datatype storage
+    sudo -u xdmod xdmod-ingestor --aggregate=storage --last-modified-start-date "$last_modified_start_date"
+  fi
+
+  sudo -u xdmod xdmod-import-csv -t names -i $REF_DIR/names.csv
+  sudo -u xdmod xdmod-ingestor
+  php $BASEDIR/scripts/create_xdmod_users.php
   dnf clean all
   rm -rf /var/cache/yum /var/cache/dnf
   rm -rf /root/xdmod /root/rpmbuild
