aadb255b5aa96b62f0f07fde67e082b9858bd6a4
shixinwu <shixinwu16@gmail.com>
2024-07-23 16:48:35 -0400

    Added testmode to supremm

--- xdmod-job-performance/entrypoint.sh
+++ xdmod-job-performance/entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 set -o pipefail
 
+tail
+
 if [ "$1" = "build" ]
 then
   dnf install -y ~/rpmbuild/RPMS/noarch/xdmod-supremm*.rpm
@@ -15,7 +17,6 @@ then
   wget -nv https://raw.githubusercontent.com/ShixinWu16/xdmod-supremm/dockersplit/tests/integration/scripts/xdmod-setup.tcl
   expect xdmod-setup.tcl | col -b || true
   rm -rf xdmod-setup.tcl
-  aggregate_supremm.sh -d
   rm -rf /root/xdmod-supremm /root/xdmod
   acl-config
   /usr/sbin/postfix start
@@ -23,6 +24,28 @@ then
   rm -f /var/run/httpd/httpd.pid
   /usr/sbin/httpd -DFOREGROUND
 fi
+
+if [ "$1" = "testbuild" ]
+then
+  dnf install -y ~/rpmbuild/RPMS/noarch/xdmod-supremm*.rpm
+  ~/bin/importmongo.sh
+  wget -nv https://raw.githubusercontent.com/ubccr/xdmod-supremm/xdmod11.0/tests/integration/scripts/mongo_auth.mongojs
+  mongo mongodb://root:admin@mongodb:27017 mongo_auth.mongojs
+  rm -rf mongo_auth.mongojs
+  # wget -nv https://github.com/ubccr/xdmod-supremm/blob/xdmod11.0/tests/integration/scripts/xdmod-setup.tcl && \
+  # wget -nv https://raw.githubusercontent.com/ubccr/xdmod-supremm/xdmod11.0/tests/integration/scripts/xdmod-setup.tcl && \
+  wget -nv https://raw.githubusercontent.com/ShixinWu16/xdmod-supremm/dockersplit/tests/integration/scripts/xdmod-setup.tcl
+  expect xdmod-setup.tcl | col -b || true
+  rm -rf xdmod-setup.tcl
+  aggregate_supremm.sh
+  rm -rf /root/xdmod-supremm /root/xdmod
+  acl-config
+  /usr/sbin/postfix start
+  php-fpm
+  rm -f /var/run/httpd/httpd.pid
+  /usr/sbin/httpd -DFOREGROUND
+fi
+
 
 if [ "$1" = "start" ]
 then
